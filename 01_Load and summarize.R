daeb0012e717203f7100c61b1ac83e8de1b4be05
Daniel Bontempo <daniel.bontempo@ttu.edu>
2019-03-11 16:50:35 -0500

    Add files via upload

---
+++ 01_Load and summarize.R
@@ -0,0 +1,12 @@
+library(PcAux)
+
+obj <- readRDS("extractDataOut-SC-20181204.rds")
+str(obj)
+
+print("The call history is:")
+print(obj$call)
+
+# vissualize components
+plot(obj$rSquared$lin)
+
+# some change
