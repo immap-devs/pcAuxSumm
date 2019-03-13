2fc66397a837584d76007ed83fe03c300fea5d9a
pavel <pavelppanko@gmail.com>
2019-03-13 11:12:22 -0500

    Adding more script

--- 01_Load and summarize.R
+++ 01_Load and summarize.R
@@ -12,3 +12,18 @@ plot(obj$rSquared$lin)
 
 # some change
 plot(obj$rSquared$lin)
+
+library(PcAux)
+
+##My comment
+obj <- readRDS("extractDataOut-SC-20181204.rds")
+str(obj)
+
+print("The call history is:")
+print(obj$call)
+
+# vissualize components
+plot(obj$rSquared$lin)
+
+# some change
+plot(obj$rSquared$lin)
